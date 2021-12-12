424e3a3455ccde6f3cc2da9f40ef93f6aeb880cc
Altangerel Ganbaatar <mr.tuulai@gmail.com>
2021-12-12 17:50:44 -0600

    Added AddressController

--- src/main/java/com/example/ea_final_project/service/IAddressService.java
+++ src/main/java/com/example/ea_final_project/service/IAddressService.java
@@ -2,6 +2,11 @@ package com.example.ea_final_project.service;
 
 import com.example.ea_final_project.model.Address;
 
+import java.util.List;
+
 public interface IAddressService {
+    public List<Address> findAll();
+    public Address findById(Integer id) ;
     public Address create(Address address);
+    public Address update(Address address);
 }
