660db811c9648d7b9cb82aeaeb3242d06b720809
felipe-zg <felipe_zeba@outlook.com>
2021-12-12 10:38:27 -0600

    feat: add Faculty controller

--- src/main/java/com/example/ea_final_project/Application.java
+++ src/main/java/com/example/ea_final_project/Application.java
@@ -3,6 +3,9 @@ package com.example.ea_final_project;
 import com.example.ea_final_project.model.*;
 import com.example.ea_final_project.model.utils.Semester;
 import com.example.ea_final_project.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,46 +13,49 @@ import org.springframework.context.ApplicationContext;
 import java.time.LocalDate;
 
 @SpringBootApplication
-public class Application {
+public class Application implements ApplicationRunner {
+    @Autowired AddressService addressService;
+    @Autowired StudentService studentService;
+    @Autowired  CourseService courseService;
+    @Autowired FacultyService facultyService;
+    @Autowired AcademicBlockService blockService;
+    @Autowired RegistrationGroupService registrationGroupService;
+    @Autowired CourseOfferingService courseOfferingService;
+    @Autowired RegistrationService registrationService;
 
     public static void main(String[] args) {
-        ApplicationContext context =  SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
+    }
 
-        IAddressService addressService = context.getBean(AddressService.class);
-        IStudentService studentService = context.getBean(StudentService.class);
-        ICourseService courseService = context.getBean(CourseService.class);
-        IFacultyService facultyService = context.getBean(FacultyService.class);
-        IAcademicBlockService blockService = context.getBean(AcademicBlockService.class);
-        IRegistrationGroupService registrationGroupService = context.getBean(RegistrationGroupService.class);
-        ICourseOfferingService courseOfferingService = context.getBean(CourseOfferingService.class);
-        IRegistrationService registrationService = context.getBean(RegistrationService.class);
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
 
-        State state = new State("Iowa");
-        Country country = new Country("United States");
-        Address address = new Address("1000 N 4th St", "Fairfield", "52557", state, country);
+//        State state = new State("Iowa");
+//        Country country = new Country("United States");
+//        Address address = new Address("1000 N 4th St", "Fairfield", "52557", state, country);
 //        addressService.create(address);
-
-        Student student = new Student("5635625", "John", "john@gmail.com", address, address);
+//
+//        Student student = new Student("5635625", "John", "john@gmail.com", address, address);
 //        studentService.create(student);
-
-        Course course = new Course("ASD", "CS503", "Advanced software development");
+//
+//        Course course = new Course("ASD", "CS503", "Advanced software development");
 //        courseService.create(course);
-
-        Faculty faculty = new Faculty("John", "Cole", "John@gmail.com", "Professor");
+//
+//        Faculty faculty = new Faculty("John", "Cole", "John@gmail.com", "Professor");
 //        facultyService.create(faculty);
-
-        AcademicBlock block = new AcademicBlock("December 2021", "2021-12A-12D-", Semester.WINTER, LocalDate.now(), LocalDate.now());
+//
+//        AcademicBlock block = new AcademicBlock("December 2021", "2021-12A-12D-", Semester.WINTER, LocalDate.now(), LocalDate.now());
 //        blockService.create(block);
-
-        RegistrationGroup group = new RegistrationGroup();
-        group.addStudent(student);
-        group.addAcademicBlock(block);
+//
+//        RegistrationGroup group = new RegistrationGroup();
+//        group.addStudent(student);
+//        group.addAcademicBlock(block);
 //        registrationGroupService.create(group);
-
-        CourseOffering courseOffering = new CourseOffering("2021-12A-12D", 40, course, faculty, block);
+//
+//        CourseOffering courseOffering = new CourseOffering("2021-12A-12D", 40, course, faculty, block);
 //        courseOfferingService.create(courseOffering);
 
-        Registration registration = new Registration(courseOffering, student);
-        registrationService.create(registration);
+//        Registration registration = new Registration(courseOffering, student);
+//        registrationService.create(registration);
     }
 }
--- src/main/java/com/example/ea_final_project/service/FacultyService.java
+++ src/main/java/com/example/ea_final_project/service/FacultyService.java
@@ -5,13 +5,31 @@ import com.example.ea_final_project.repository.FacultyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class FacultyService implements IFacultyService{
     @Autowired
     private FacultyRepository repository;
 
     @Override
+    public List<Faculty> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Faculty findById(Integer id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @Override
     public Faculty create(Faculty faculty) {
         return repository.save(faculty);
     }
+
+    @Override
+    public Faculty update(Faculty manufacturer) {
+        return repository.save(manufacturer);
+    }
+
 }
