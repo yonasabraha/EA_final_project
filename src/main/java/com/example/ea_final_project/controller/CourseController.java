7dbebb1c55846eb65422713837357053dddffbc8
Solia-dove <solime27@gmail.com>
2021-12-12 22:02:20 -0600

    test commit

---
+++ src/main/java/com/example/ea_final_project/controller/CourseController.java
@@ -0,0 +1,9 @@
+package com.example.ea_final_project.controller;
+
+import com.example.ea_final_project.model.Course;
+
+public class CourseController {
+    public Course findAll(){
+        return null;
+    }
+}
